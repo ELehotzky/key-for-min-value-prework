f0865c0f0da895cc3f6e9ac5be5d7f394ed43c30
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-13 16:03:41 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,22 +8,9 @@ def key_for_min_value(name_hash)
     if smallest_value == nil || value < smallest_value
       smallest_value = value
       smallest_key = key
+    else
+      nil
+    end
 end
-
-
-
-
-
- def key_for_min_value(name_hash)
-+  lowest_key = nil
-+  lowest_value = nil
-+  name_hash.each do |group, item|
-+    if lowest_value == nil || item < lowest_value
-+      lowest_value = item
-+      lowest_key = group
-+    else
-+      nil
-+    end
-+  end
-+  lowest_key
-+end
+smallest_key
+end
